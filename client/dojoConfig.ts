610eb7b456803bcb62cd56c36aa970545ad77cbd
joeperpetua <qwerty.perpetua@gmail.com>
2024-11-12 13:03:33 +0100

    fix manifest

--- client/dojoConfig.ts
+++ client/dojoConfig.ts
@@ -3,7 +3,7 @@ import { createDojoConfig } from "@dojoengine/core";
 import manifest_dev from "../backend/manifest_dev.json";
 import manifest_prod from "../backend/manifest_release.json";
 
-const manifest = import.meta.env.VITE_PUBLIC_SLOT_RPC ? manifest_prod : manifest_dev;
+const manifest = import.meta.env.VITE_PUBLIC_SLOT_RPC === "http://localhost:5050" ? manifest_dev : manifest_prod;
 
 export const dojoConfig = createDojoConfig({
     manifest,
